5f53e753f0f3eb6573c53deba857e512f445ef2a
Sanna Mäkinen <sanna.makinen@gmail.com>
2021-11-17 17:01:21 +0200

    Remove the annoying skip to content link animation

--- src/web/themes/portfolio/js/portfolio.js
+++ src/web/themes/portfolio/js/portfolio.js
@@ -1,1 +1,1 @@
-!function(e){Drupal.behaviors.portfolio={attach:function(n,o){var t=e(".skip-link"),i=e("#main-content"),a=e(".main-menu-toggle"),c=e(".main-menu-nav");t.click(function(n){n.preventDefault(),e("html, body").animate({scrollTop:i.offset().top},1e3,function(){if((location.hash=i).focus(),i.is(":focus"))return!1;i.attr("tabindex","-1"),i.focus()})}),a.click(function(){c.toggleClass("show")})}}}(jQuery);
+!function(t){Drupal.behaviors.portfolio={attach:function(n,o){var a=t(".main-menu-toggle"),i=t(".main-menu-nav");a.click(function(){i.toggleClass("show")})}}}(jQuery);
